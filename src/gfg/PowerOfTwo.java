63673421bed07f964f65524be717772d93d1ebe8
abhinav3254 <abhinav3254@gmail.com>
2022-10-09 20:59:15 +0530

    gfg and leet-code

--- src/gfg/PowerOfTwo.java
+++ src/gfg/PowerOfTwo.java
@@ -38,4 +38,21 @@ public class PowerOfTwo {
         }
         return true;
     }
+
+    public static boolean isPowerOfFour(long n){
+
+        // Your code here
+        if (n==0) {
+            return false;
+        }
+
+        while(n!=1) {
+            if (n%4!=0) {
+                return false;
+            }
+            n = n/4;
+        }
+        return true;
+    }
+
 }
