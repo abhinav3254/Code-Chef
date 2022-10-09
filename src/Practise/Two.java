e197430a75521b57f4023cb4ba68488dd863640b
abhinav3254 <abhinav3254@gmail.com>
2022-10-09 19:40:23 +0530

    changes

--- src/Practise/Two.java
+++ src/Practise/Two.java
@@ -3,6 +3,5 @@ package Practise;
 public class Two {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("create a pull request");
     }
 }
