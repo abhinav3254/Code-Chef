aaaafd07f3cee2e609b2947056d2e676f4733baf
abhinav3254 <abhinav3254@gmail.com>
2022-10-09 20:56:25 +0530

    gfg and leet-code

---
+++ src/gfg/PowerOfTwo.java
@@ -0,0 +1,41 @@
+package gfg;
+
+public class PowerOfTwo {
+
+    public static void main(String[] args) {
+        boolean n = isPowerOfTwo(16);
+        System.out.println(n);
+    }
+
+    public static boolean isPowerOfTwo(long n){
+
+        // Your code here
+        if (n==0) {
+            return false;
+        }
+
+        while(n!=1) {
+            if (n%2!=0) {
+                return false;
+            }
+            n = n/2;
+        }
+         return true;
+    }
+
+    public static boolean isPowerOfThree(long n){
+
+        // Your code here
+        if (n==0) {
+            return false;
+        }
+
+        while(n!=1) {
+            if (n%3!=0) {
+                return false;
+            }
+            n = n/3;
+        }
+        return true;
+    }
+}
