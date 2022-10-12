1d66f02a82df9722d2d8dca96c39b31a96b7182d
abhinav3254 <abhinav3254@gmail.com>
2022-10-12 13:26:20 +0530

    Generics Linked List

---
+++ src/Practise/Three.java
@@ -0,0 +1,46 @@
+package Practise;
+
+public class Three<T> {
+
+    Node<T> head = null;
+
+    public void insertData (T data) {
+        Node<T> node = new Node<>(data);
+
+        if (head == null) {
+            head = node;
+        } else {
+            Node<T> temp = head;
+            while (temp.next!=null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+
+    public void printData () {
+        Node<T> temp = head;
+        while (temp!=null) {
+            System.out.print(temp.data+" --> ");
+            temp = temp.next;
+        }
+        System.out.println("X");
+    }
+
+    public static void main(String[] args) {
+        Three<Integer> three = new Three<>();
+        three.insertData(10);
+        three.insertData(20);
+        three.printData();
+    }
+
+    class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
